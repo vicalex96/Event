57a24515a0882bf1c2b30518f0d38ea35806e51d
vicalex96 <35385231+vicalex96@users.noreply.github.com>
2018-12-29 13:15:58 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,25 +6,18 @@ app.use(express.static(path.join(__dirname)));
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/img", express.static(__dirname + '/imagen'));
 app.use("/js", express.static(__dirname + '/scripts'));
-app.use("/php", express.static(__dirname + '/php'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/home.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // add other routes below
 app.get('/sign-in', function (req, res) {
-  res.sendFile(path.join(__dirname + '/iniciosesion.html'));
-});
-app.get('/sing-up', function (req, res) {
-  res.sendFile(path.join(__dirname + '/registro.html'));
-});
-app.get('/perfil_de_usuario', function (req, res) {
-  res.sendFile(path.join(__dirname + '/perfilUsuario.html'));
-});
-app.get('/recuperar_contrasena', function (req, res) {
-  res.sendFile(path.join(__dirname + '/recuperarPass.html'));
+  res.sendFile(path.join(__dirname + '/inicio_sicsion.html'));
 });
 
+app.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 app.listen(process.env.PORT || 8080);
